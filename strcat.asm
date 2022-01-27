17487080f36540556dce81f0d89ac12020b5212c
OrShazam <50451273+OrShazam@users.noreply.github.com>
2022-01-27 22:01:12 +0200

    not my code

---
+++ strcat.asm
@@ -0,0 +1,19 @@
+
+mov edi, [esp+8]
+or ecx, 0FFFFFFFFh
+xor eax,eax 
+repne scasb 
+not ecx 
+sub edi, ecx
+mov esi, edi
+mov edi, [esp+4]
+mov ebx, ecx 
+or ecx, 0FFFFFFFFh
+repne scasb 
+mov ecx, ebx
+shr ecx, 2
+rep movsd
+mov ecx, ebx
+and ecx, 3
+rep movsb
+ret 8
