9a1ba16eda9744639db8f88498ba55e3e39d4166
OrShazam <50451273+OrShazam@users.noreply.github.com>
2022-01-31 09:06:18 +0200

    Update strncmp.asm

--- strncmp.asm
+++ strncmp.asm
@@ -9,6 +9,7 @@ xor eax, eax
 repne scasb
 neg ecx
 add ecx, ebx
+mov edi, esi
 mov esi, [ebp+8]
 repe cmpsb
 mov al, [esi-1]
@@ -16,6 +17,7 @@ xor ecx, ecx
 cmp al, [edi-1]
 ja above
 jz epilog 
+dec ecx 
 dec ecx 
 above:
 not ecx
