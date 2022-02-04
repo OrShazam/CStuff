dcba2c0eb7dd4c0564a880bf1db82d15f5bd954a
OrShazam <50451273+OrShazam@users.noreply.github.com>
2022-02-04 15:35:08 +0200

    Update strncmp.asm

--- strncmp.asm
+++ strncmp.asm
@@ -1,17 +1,17 @@
 ;compiler generated code
 ;just generalized it 
 strncmp:
-mov ecx, [ebp+0Ch]
+mov ecx, [esp+0Ch]
 jecxz epilog
 mov ebx, ecx
-mov edi, [ebp+4]
+mov edi, [esp+4]
 mov esi, edi
 xor eax, eax
 repne scasb
 neg ecx
 add ecx, ebx
 mov edi, esi
-mov esi, [ebp+8]
+mov esi, [esp+8]
 repe cmpsb
 mov al, [esi-1]
 xor ecx, ecx
