ae3d948b0345c1add1743f836ac0688df05425c0
OrShazam <50451273+OrShazam@users.noreply.github.com>
2022-02-04 15:35:25 +0200

    Update strrchr.asm

--- strrchr.asm
+++ strrchr.asm
@@ -1,14 +1,14 @@
 ;compiler generated code
 ;just generalized it 
 strrchr:
-mov edi, [ebp+8]
+mov edi, [esp+8]
 xor eax, eax
 or ecx, 0FFFFFFFFh
 repne scasb
 inc ecx 
 neg ecx
 dec edi
-mov al, [ebp+4]
+mov al, [esp+4]
 std 
 repne scasb
 inc edi 
