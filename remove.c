ce79e976222e7795c7b04bd1dbc3bdf436e6d75e
OrShazam <50451273+OrShazam@users.noreply.github.com>
2022-06-18 14:40:08 +0300

    Update remove.c

--- remove.c
+++ remove.c
@@ -1,10 +1,30 @@
 int remove(void* start, void* end, int elem_size, void* value){
 	int cnt = 0;
-	while (start != end){
+	void* ptr = start;
+	while (ptr != end){
 		if (memcmp(start,value,elem_size)){
-			memcpy(start + cnt++ * elem_size, start, elem_size);
+			memcpy(start + cnt, ptr, elem_size);
+			cnt += elem_size;
 		}
-		start += elem_size;
+		ptr += elem_size;
 	}
 	return cnt;
+}
+
+int remove_v2(void* start, void* end, int elem_size, void* value){
+	// 'lazy' approach
+	int cnt = 0, sum = 0;
+	void* cpy = start, ptr = start;
+	while (ptr != end){
+		if (!memcmp(ptr,value,elem_size)){
+			memcpy(start + sum, cpy, cnt);
+			sum += cnt;
+			cpy = ptr + elem_size; cnt = 0;
+		}
+		else {
+			cnt += elem_size;
+		}
+		ptr += elem_size;
+	}
+	return sum;
 }
